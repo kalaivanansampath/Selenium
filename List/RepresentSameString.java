c537a4c93466f84117b8fb2946460f8c5f9d5a7b
kalaivanansampath <121231547+kalaivanansampath@users.noreply.github.com>
2022-12-28 16:10:29 +0530

    Update RepresentSameString.java

--- List/RepresentSameString.java
+++ List/RepresentSameString.java
@@ -5,8 +5,8 @@ public class RepresentSameString {
 	public static void main(String args[]) {
 		String word1[] = { "ab", "c" };
 		String word2[] = { "ab", "c" };
-		StringBuffer sb1 = new StringBuffer();
-		StringBuffer sb2 = new StringBuffer();
+		StringBuilder sb1 = new StringBuilder();
+		StringBuilder sb2 = new StringBuilder(); 
 		for (int i = 0; i < word1.length; i++) {
 			sb1.append(word1[i]);
 		}
