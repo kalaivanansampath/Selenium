fff975d54f08a3e48882a430550066195a519fa2
kalaivanansampath <121231547+kalaivanansampath@users.noreply.github.com>
2022-12-30 15:01:01 +0530

    Update MapLuckyInteger.java
    Map name renaming

--- List/MapLuckyInteger.java
+++ List/MapLuckyInteger.java
@@ -12,21 +12,21 @@ public class MapLuckyInteger {
 	public static void main(String[] args) {
 		int[] no = { 2, 2, 3, 4 };
 
-		Map<Integer, Integer> iterationNo = new TreeMap<Integer, Integer>();
+		Map<Integer, Integer> map = new TreeMap<Integer, Integer>();
 
 		for (int i = 0; i < no.length; i++) {
-			if (iterationNo.containsKey(no[i])) {
-				Integer existingcount = iterationNo.get(no[i]);
-				iterationNo.put(no[i], existingcount + 1);
+			if (map.containsKey(no[i])) {
+				Integer existingcount = map.get(no[i]);
+				map.put(no[i], existingcount + 1);
 			} else {
-				iterationNo.put(no[i], 1);
+				map.put(no[i], 1);
 			}
 		}
 
 		boolean luckyNumber = true;
 		Set<Integer> set = new TreeSet<>();
 
-		for (Map.Entry<Integer, Integer> entry : iterationNo.entrySet())
+		for (Map.Entry<Integer, Integer> entry : map.entrySet())
 			if (entry.getKey() == entry.getValue()) {
 				set.add(entry.getValue());
 			}
