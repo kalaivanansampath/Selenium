ddbdbd33df9248b36ed626d183472d8a35e4a9d1
kalaivanansampath <121231547+kalaivanansampath@users.noreply.github.com>
2023-01-06 15:22:21 +0530

    Create Experience's.java
    asdasdasd

---
+++ Experience's.java
@@ -0,0 +1,28 @@
+What are Oops concept used in ur project
+Explain framrwork
+Cucumber
+Dryrun usage
+Scenario outline with example
+When give single parameter do we need pipeline
+How will you group testcase for sanity, regression
+Page factory usage
+What is framework
+Benefits of page object modal
+Hook methods
+What is hook
+Monochrome usage
+Selenium
+How to handle window
+Frames
+What is frame tag
+How to find all broken links
+How to find all links in web page
+How to take screenhots
+Possible way to upload a file ( sendkeys and robot class?
+2 usages of action class
+2 usage of Javascript executor
+2 usage of robot lass
+Possible way to send text( sendkeys, Javascript executor)
+What is webdriver driver=new chrome driver()
+How to verify webelements
+How to verify button color
