529d3ddf3c2bfc2e4931e5b994097fef9f1f42ed
kalaivanansampath <121231547+kalaivanansampath@users.noreply.github.com>
2023-03-28 16:28:01 +0530

    Update SameCharactersCount.java

--- BasicPrograms/SameCharactersCount.java
+++ BasicPrograms/SameCharactersCount.java
@@ -6,24 +6,21 @@ import java.util.TreeMap;
 public class SameCharactersCount {
 	
 	public void CharacterCount(String str, int distinct) {
-		
-	
-		for (int j = 0; j < str.length(); j++) {
-
+		int j = 0;
+		while (j < str.length()) {
 			for (int k = 0; k < str.length(); k++) {
-
 				if (str.charAt(j) == str.charAt(k)) {
 					distinct++;
-
 				}
 			}
-			System.out.println(str.charAt(j) + "--" + distinct);
-			// String str1=String.valueOf(str.charAt(j)).trim();
+			System.out.println(str.charAt(j) + "" + distinct);
 			String str1 = Character.toString(str.charAt(j));
 			str = str.replaceAll(str1, "");
 			distinct = 0;
-	}}
-	
+
+		}
+	}
+
 	public void CharacterCountMap(String str, int distinct) {
 		char[] charArray = str.toCharArray();
 		Map<Character, Integer> map = new TreeMap<Character, Integer>();
@@ -35,30 +32,16 @@ public class SameCharactersCount {
 				map.put(charArray[i], 1);
 			}
 		}
-		System.out.println("MAP"+map);		
+		System.out.println("MAP" + map);
 	}
-	
+
 	public static void main(String[] args) {
 		String str = "kalaivanan";
 		int distinct = 0;
-		
+
 		SameCharactersCount obj = new SameCharactersCount();
 		obj.CharacterCount(str, distinct);
 		obj.CharacterCountMap(str, distinct);
-
-		for(int i=0; i<str.length(); i++) {
-			for(int j=0;j<str.length();j++) {
-				if(str.charAt(i)==str.charAt(j)) {
-					distinct++;
-				}}
-				System.out.println(str.charAt(i) + "--"+ distinct);
-				String str2 = Character.toString(str.charAt(i));
-				 str = str.replaceAll(str2, "");
-				 distinct=0;
-			
-		}
-
-		
 
 	}
 
