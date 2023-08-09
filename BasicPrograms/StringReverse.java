e4ab95b5c51fedcc3a756e4998f7f5fcb96bfd3b
kalaivanansampath <121231547+kalaivanansampath@users.noreply.github.com>
2023-08-09 15:57:56 +0530

    Update StringReverse.java

--- BasicPrograms/StringReverse.java
+++ BasicPrograms/StringReverse.java
@@ -16,13 +16,24 @@ public class StringReverse {
         System.out.println(input1);
     }
 
+
+	public static String reverse(String s)
+		{
+		String rev="";
+		for(int j=s.length();j>0;--j)
+		{
+		rev=rev+(s.charAt(j-1)); 
+		}
+	return rev;
+	}
+
 	public static void main(String[] args) {
 		String str = "Java Application is used in Automation";
 		String reverse ="";
 		
 		StringReverse obj = new StringReverse();
 		obj.Reverese(str);
-		
+		reverse(str);
 		for(int i=0; i<str.length();i++) {
 			char ch = str.charAt(i);
 			reverse = ch+ reverse;
