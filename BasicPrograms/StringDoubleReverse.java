bcb3d9618131d96164309b4ff91b8923bf77144d
kalaivanansampath <121231547+kalaivanansampath@users.noreply.github.com>
2023-08-21 12:59:38 +0530

    Update StringDoubleReverse.java
    Different approach

--- BasicPrograms/StringDoubleReverse.java
+++ BasicPrograms/StringDoubleReverse.java
@@ -10,33 +10,64 @@ public class StringDoubleReverse {
 		}
 		System.out.println(reverse);
 	}
-	
+
 	public static void RevereseSingleString(String str) {
-	String[] strSplit = str.split("\\s");
-	int strLength = strSplit.length;
-	LinkedList<String> list = new LinkedList<String>();
-	for (int i = 0; i < strLength; i++) {
-		list.add(strSplit[i]);
+		String[] strSplit = str.split("\\s");
+		int strLength = strSplit.length;
+		LinkedList<String> list = new LinkedList<String>();
+		for (int i = 0; i < strLength; i++) {
+			list.add(strSplit[i]);
+		}
+
+		for (int j = 0; j < list.size(); j++) {
+			String str2 = list.get(j);
+			String reverse = "";
+			for (int i = 0; i < str2.length(); i++) {
+				char ch = str2.charAt(i);
+				reverse = ch + reverse;
+			}
+			System.out.print(reverse + " ");
+		}
+
 	}
 
-	for (int j = 0; j < list.size(); j++) {
-		String str2 = list.get(j);
-		String reverse = "";
-		for (int i = 0; i < str2.length(); i++) {
-			char ch = str2.charAt(i);
-			reverse = ch + reverse;
+	public static void RevereseMinus(String s) {
+
+		String rev = "";
+		for (int j = s.length(); j > 0; --j) {
+			rev = rev + (s.charAt(j - 1));
 		}
-		System.out.print(reverse + " ");
+		System.out.println();
+		System.out.println("minus: " + rev);
 	}
-	
+
+	public static void Reverese(String str) {
+
+		String[] strSplit = str.split("\\s");
+		int strLength = strSplit.length;
+		LinkedList<String> list = new LinkedList<String>();
+		for (int i = 0; i < strLength; i++) {
+			list.add(strSplit[i]);
+		}
+
+		for (int j = 0; j < list.size(); j++) {
+			String s = list.get(j);
+			String rev1 = "";
+			for (int j1 = s.length(); j1 > 0; --j1) {
+				rev1 = rev1 + (s.charAt(j1 - 1));
+			}
+			System.out.print(rev1 + " ");
+		}
+
 	}
 
 	public static void main(String[] args) {
 		String str = "This is kalaivanan";
-		
+
 		RevereseString(str);
 		RevereseSingleString(str);
-		
+		RevereseMinus(str);
+		Reverese(str);
 
 	}
 
