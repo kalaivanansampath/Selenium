237263d3a4d2e35d4308f36d9eb2db91b2003318
kalaivanansampath <121231547+kalaivanansampath@users.noreply.github.com>
2025-07-02 13:42:34 +0530

    Update WebTable.java

--- SeleniumPrograms/WebTable.java
+++ SeleniumPrograms/WebTable.java
@@ -11,6 +11,10 @@ import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WebTable {
 
 	public static void main(String[] args) {
